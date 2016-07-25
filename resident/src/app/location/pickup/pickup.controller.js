18342d64e19c8b4eb83a07b2cd69e17b98b48cdd
Emerson Mellado <emersonmellado@gmail.com>
2016-07-25 19:07:24 -0300

    Heroku deploy

--- resident/src/app/location/pickup/pickup.controller.js
+++ resident/src/app/location/pickup/pickup.controller.js
@@ -1,36 +1,60 @@
 (function() {
-    'use strict';
-    angular.module('bProject')
-        .controller('PickupController', PickupController);
+  'use strict';
+  angular.module('bProject')
+    .controller('PickupController', PickupController);
 
-    function PickupController() {
-        var vm = this;
-        vm.formData = {};
+  function PickupController($auth) {
+    var vm = this;
+    vm.formData = {};
 
-        vm.dateConfig = {
-            format: 'mm-dd-yyyy'
-        };
+    vm.dateConfig = {
+      format: 'mm-dd-yyyy'
+    };
 
-        vm.timeConfig = {
-            min: [7, 0],
-            max: [18, 0]
-        }
+    vm.user = $auth.getPayload();
 
-        // function to process the form
-        vm.processForm = function() {
-            alert('awesome!');
-        };
-
-        vm.valuationDatePickerOpen = function($event) {
-            if ($event) {
-                $event.preventDefault();
-                $event.stopPropagation(); // This is the magic
-            }
-            vm.valuationDatePickerIsOpen = true;
-        };
-
-        vm.valuationDate = new Date();
-        vm.valuationDatePickerIsOpen = false;
+    vm.timeConfig = {
+      min: [7, 0],
+      max: [18, 0]
     }
+
+    // function to process the form
+    vm.processForm = function() {
+      var pickup = {
+        "requester": "",
+        "address": {
+          "street": vm.formData.address,
+          "city": "Vancouver",
+          "state": "CA",
+          "zip": vm.formData.zip,
+          "location": {
+            "type": vm.formData.location,
+            "coordinates": [
+              0
+            ]
+          }
+        },
+        "time": vm.formData.date + vm.formData.time,
+        "instructions": vm.formData.instructions,
+        "items": [{
+          "type": vm.formData.type,
+          "packageSize": vm.formData.size,
+          "quantity": vm.formData.size
+        }]
+      }
+      console.log("vm.formData", vm.formData);
+    };
+
+    vm.valuationDatePickerOpen = function($event) {
+      if ($event) {
+        $event.preventDefault();
+        $event.stopPropagation(); // This is the magic
+      }
+      vm.valuationDatePickerIsOpen = true;
+    };
+
+    vm.valuationDate = new Date();
+    vm.valuationDatePickerIsOpen = false;
+  }
 
 })();
